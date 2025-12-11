3046695abe4391eb94fc46d33b66b6eeeaf177dd
gourav bhatt <gourav5@gmail.com>
2025-12-11 23:57:31 +0530

    final commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   try {
     setLoading(true);
     setPrediction("");  
-    const res = await fetch("http://127.0.0.1:5000/predict", {
+    const res = await fetch("http://127.0.0.1:10000/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message }),
